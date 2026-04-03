e21334fed1229528919d4989b3861f160856ac38
rustycod <naveen.paliwal@gmail.com>
2026-04-03 16:58:17 +0530

    Created BinarySearch.java

---
+++ LevenshteinDistance/src/org/sample/search/BinarySearch.java
@@ -0,0 +1,45 @@
+package org.sample.search;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		
+		int[] array1 = new int[]{1, 3, 5, 7, 8}; // ans 2
+		System.out.println("result is: "+BinarySearch.binarySearch(array1, 5));
+		
+		int[] array2 = new int[]{1, 2, 3, 4, 5, 6, 7}; // ans -1
+		System.out.println("result is: "+BinarySearch.binarySearch(array2, 0));
+		
+		int[] array3 = new int[]{10, 20}; // ans 1
+		System.out.println("result is: "+BinarySearch.binarySearch(array3, 20));		
+		
+	}
+	
+    public static int binarySearch(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        /*
+         * In binary search iteration method ‘=’ in condition “start <= end” is required as there could 
+         * be only one number present and mid should point to this. 
+         * ‘Return’ in mid condition will help in terminating the while loop in this case.
+         * 
+         */
+        
+        while(start <= end) { 
+
+            int mid = (start + end) / 2;
+            if(arr[mid] == target) {
+                return mid;
+            } else if(arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }                
+        
+        return -1;
+    }
+	
+}
