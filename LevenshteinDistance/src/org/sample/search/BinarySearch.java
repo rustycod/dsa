536dc4a1862dc0b13bd9f51e96f614f7b835b094
rustycod <naveen.paliwal@gmail.com>
2026-04-15 17:41:33 +0530

    Update BinarySearch.java

--- LevenshteinDistance/src/org/sample/search/BinarySearch.java
+++ LevenshteinDistance/src/org/sample/search/BinarySearch.java
@@ -139,6 +139,42 @@ public class BinarySearch {
     }
 
 
+	public int findMaximumInBitonicArray(int[] arr) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = 0;
+        
+        
+        while(start < end) {
+            
+            mid = start + (end - start) / 2;
+            
+            if(mid > 0 && mid < (arr.length - 1)) { // important edge case
+                if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
+                    return arr[mid];
+                }
+            
+                if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]) {
+                    start = mid + 1;
+                } 
+            
+                if(arr[mid] < arr[mid-1] && arr[mid] > arr[mid+1]) {
+                    end = mid - 1;
+                }                
+            } else if(mid == 0 && arr[mid] > arr[mid+1]) {
+                return arr[mid];
+            } else if(mid == (arr.length - 1) && arr[mid] > arr[mid-1]) {
+                return arr[mid];
+            } 
+
+        }
+        
+        return arr[start];
+        
+    }
+
+
 	
 	
 }
