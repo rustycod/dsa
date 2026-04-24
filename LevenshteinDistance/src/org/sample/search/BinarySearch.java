fb6bffd99f3f77f486c29bc8d2cf47f10fa5bcb0
rustycod <naveen.paliwal@gmail.com>
2026-04-24 20:20:47 +0530

    Update BinarySearch.java

--- LevenshteinDistance/src/org/sample/search/BinarySearch.java
+++ LevenshteinDistance/src/org/sample/search/BinarySearch.java
@@ -328,36 +328,27 @@ public class BinarySearch {
 	
 	public int findMaximumInBitonicArray(int[] arr) {
         
-        int start = 0;
-        int end = arr.length - 1;
-        int mid = 0;
-        
-        
-        while(start < end) {
-            
-            mid = start + (end - start) / 2;
-            
-            if(mid > 0 && mid < (arr.length - 1)) { // important edge case
-                if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
-                    return arr[mid];
-                }
-            
-                if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]) {
-                    start = mid + 1;
-                } 
-            
-                if(arr[mid] < arr[mid-1] && arr[mid] > arr[mid+1]) {
-                    end = mid - 1;
-                }                
-            } else if(mid == 0 && arr[mid] > arr[mid+1]) {
-                return arr[mid];
-            } else if(mid == (arr.length - 1) && arr[mid] > arr[mid-1]) {
-                return arr[mid];
-            } 
+    	int left = 0;
+    	int right = nums.length - 1;
 
-        }
-        
-        return arr[start];
+    	while(left <= right) {
+
+    		int mid = left + (right - left) / 2;
+
+    		// is left neighbour greater?
+    		if(mid > 0 && nums[mid] < nums[mid-1]) {
+    			right = mid - 1;
+    		}
+
+    		// is right neighbour greater?
+    		else if(mid < nums.length - 1 && nums[mid] < nums[mid+1]) {
+    			left = mid + 1;
+    		} else {
+    			return mid; // then we found the element
+    		}
+    	}
+
+    	return -1;
         
     }
 
